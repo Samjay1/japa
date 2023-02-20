0c754b780bb3eb8c56ad531ef70c3bd25b9e61fb
Samjay <barnabassampana89@gmail.com>
2023-02-20 21:27:02 +0000

    created controllers

---
+++ controllers/main.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const router = express.Router();
+
+// home
+router.get('/', (req,res)=>{
+
+})
+
+// about
+router.get('/', (req,res)=>{
+    
+})
+
+// news feed
+router.get('/', (req,res)=>{
+    
+})
+
+// faqs
+router.get('/', (req,res)=>{
+    
+})
+
+// contact us
+router.get('/', (req,res)=>{
+    
+})
+
+// sign up
+router.get('/', (req,res)=>{
+    
+})
+
+// sign in
+router.get('/', (req,res)=>{
+    
+})
+
+
+router.get('/', (req,res)=>{
+    
+})
+
+
+module.exports = router;
---
+++ controllers/resources.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+
+
+// resources-main
+router.get('/', (req,res)=>{
+    
+})
+
+// blog
+router.get('/', (req,res)=>{
+    
+})
+
+
+// documents
+router.get('/', (req,res)=>{
+    
+})
+
+// videos
+router.get('/', (req,res)=>{
+    
+})
+
+
+
+module.exports = router;
