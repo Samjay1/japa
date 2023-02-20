c672e28df1db9a921ac222f0920cabc9ff10f1e3
Samjay <barnabassampana89@gmail.com>
2023-02-20 21:28:03 +0000

    created controllers

---
+++ controllers/chats.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const router = express.Router();
+
+// chats- all groups
+router.get('/', (req,res)=>{
+    
+})
+
+// chats- my groups
+router.get('/', (req,res)=>{
+    
+})
+
+// chats- create group
+router.get('/', (req,res)=>{
+    
+})
+
+// settings
+router.get('/', (req,res)=>{
+    
+})
+
+router.get('/', (req,res)=>{
+    
+})
+
+
+module.exports = router;
