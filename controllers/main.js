f959eebbbb58cbc4fa9f309e9da89d3293f2c69d
Samjay <barnabassampana89@gmail.com>
2023-03-08 13:45:12 +0000

    forgot password first stage + email reset link

--- controllers/main.js
+++ controllers/main.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 
 const app = express();
@@ -52,9 +53,34 @@ router.get('/home', async (req,res)=>{
 });
 
 // about
-router.get('/about', (req,res)=>{
+router.get('/about', async (req,res)=>{
+
+    const posts = await prisma.post.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+        take: 1,
+      });
+      console.log('POSTS :>> ', posts);
+    const documents = await prisma.document.findMany({
+         orderBy: {
+        createdAt: 'desc',
+    },
+    take: 1,
+      });
+    const videos = await prisma.video.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+        take: 1,
+      })
     res.render('main/about',
-    {email:req.session.email || null});
+    {
+        email:req.session.email || null,
+        post:posts[0],
+        document:documents[0],
+        video:videos[0]
+    });
 });
 
 // news feed
@@ -137,6 +163,86 @@ router.all('/login', async (req,res)=>{
         success: req.flash('success')
     });
 });
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    authMethod:"PLAIN",
+    auth: {
+        // type: 'OAuth2',
+        // user: process.env.GMAIL, // generated ethereal user
+        // pass: process.env.PASS,
+        user: 'japa.run.official@gmail.com',
+        pass: 'pkeooiqhrddzmgez'
+    },            
+})
+
+// forgot password
+router.all('/forgotpassword',async(req,res)=>{
+    if(req.method === 'POST'){
+        let email = req.body.email;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+        console.log('user :>> ', user);
+
+        let mailOptions = {
+            from: 'no-reply@japa.run',
+            to: email,
+            subject: 'Password reset request - Japa.run',
+            html: `<div style="width: 80%; margin: auto;">
+
+            <img class="size-medium wp-image-4144 aligncenter" src="https://japa.run/wp-content/uploads/2022/11/japalogo-300x44.webp" alt="" width="300" height="44" />
+            
+            <hr style="border: 1px solid orange;" />
+            <p>Hello ${user.username}</p>
+            <p>You requested a password reset on your Japa.run Account</p>
+            <p>Please click the LINK below to set a new password on your account</p>
+
+            <p> <a href="https://japa.run/reset/${user.id}">https://japa.run/reset/${user.id}</a></p>
+
+            <p>If you did not initiate this request, please ignore this email and contact our Support Team immediately.</p>
+           
+            <p>Best Regards,</p>
+            <p>Japa.Run Team!</p>
+
+            <hr style="border: 1px solid orange;" />
+            
+            <div style="background-color: gray; padding: 10px 5px; margin-top: 5px; color: white;">
+            <div style="text-align: center; color: white;">
+            
+            Do not hesitate to reach out.
+            Message Us at <a style="color: orange;" href="mailto:support@japa.com">support@japa.com</a>
+            
+            © 2023 Japa.Run - Join the conversation!
+            <a style="color: orange;" href="https://japa.run">www.japa.run</a>
+            
+            </div>
+            </div>
+            </div>`
+          };
+          transporter.sendMail(mailOptions, function(error, info){
+              if(error){
+                res.send('error sending email')
+                return;
+              }
+              else{
+                res.send('email sent')
+              }
+          })
+    }else{
+        res.render('main/forgotpassword',
+        {
+            email:req.session.email || null,
+            error: req.flash('error'),
+            success: req.flash('success')
+        })
+    }
+})
 
 
 module.exports = router;
